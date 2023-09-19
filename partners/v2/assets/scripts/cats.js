7f884188d3dadab2ad86b5824961f7d4fb88dbda
Bruno Sastre <54677414+bsastregx@users.noreply.github.com>
2023-09-19 03:32:17 -0300

    update partners v2

---
+++ partners/v2/assets/scripts/cats.js
@@ -0,0 +1,478 @@
+const gxFilterData = {
+  conf: {
+    typeSingular: {
+      en: "partner",
+      es: "socio",
+      pt: "parceiro",
+    },
+    typePlural: {
+      en: "partners",
+      es: "socios",
+      pt: "parceiros",
+    },
+    cardsPerLoad: 6,
+    desktopLgWidth: 1138,
+  },
+  filterTitle: {
+    en: "Find a partner",
+    es: "Encontrar un socio",
+    pt: "Encontrar um parceiro",
+  },
+  cats: [
+    /*categories*/
+    {
+      type: "category",
+      label: {
+        en: "category",
+        es: "categoría",
+        pt: "categoria",
+      },
+      cats: [
+        {
+          value: "C128438-5676",
+          label: {
+            en: "diamond",
+            es: "diamond",
+            pt: "diamond",
+          },
+        },
+        {
+          value: "C128438-5691",
+          label: {
+            en: "platinum",
+            es: "platinum",
+            pt: "platinum",
+          },
+        },
+        {
+          value: "C128438-5692",
+          label: {
+            en: "gold",
+            es: "gold",
+            pt: "gold",
+          },
+        },
+        {
+          value: "C128438-5693",
+          label: {
+            en: "silver",
+            es: "silver",
+            pt: "silver",
+          },
+        },
+        {
+          value: "C128438-5698",
+          label: {
+            en: "member",
+            es: "member",
+            pt: "member",
+          },
+        },
+      ],
+    },
+    /*types*/
+    {
+      type: "types",
+      label: {
+        en: "partner types",
+        es: "tipos de socios",
+        pt: "tipos de parceiros",
+      },
+      cats: [
+        {
+          value: "C128438-5697",
+          label: {
+            en: "solution partner",
+            es: "socio de soluciones",
+            pt: "parceiro de soluções",
+          },
+        },
+        {
+          value: "C128438-5699",
+          label: {
+            en: "integration partner",
+            es: "socio de integración",
+            pt: "parceiro de integração",
+          },
+        },
+        {
+          value: "C128438-5700",
+          label: {
+            en: "platform partner",
+            es: "socio de plataforma",
+            pt: "parceiro de plataforma",
+          },
+        },
+        {
+          value: "C128438-5701",
+          label: {
+            en: "software development partner",
+            es: "socio de desarrollo de software",
+            pt: "parceiro de desenvolvimento de software",
+          },
+        },
+      ],
+    },
+    /*industries*/
+    {
+      type: "industries",
+      label: {
+        en: "industries",
+        es: "industrias",
+        pt: "industrias",
+      },
+      cats: [
+        {
+          value: "C128438-2672",
+          label: {
+            en: "finance",
+            es: "finanzas",
+            pt: "finanças",
+          },
+        },
+        {
+          value: "C128438-5702",
+          label: {
+            en: "information technology",
+            es: "tecnologías de la información",
+            pt: "tecnologia da informação",
+          },
+        },
+        {
+          value: "C128438-5682",
+          label: {
+            en: "insurance",
+            es: "seguros",
+            pt: "seguros",
+          },
+        },
+        {
+          value: "C128438-5703",
+          label: {
+            en: "industry and business",
+            es: "industria y comercio",
+            pt: "indústria e negócios",
+          },
+        },
+        {
+          value: "C128438-5666",
+          label: {
+            en: "healthcare",
+            es: "salud",
+            pt: "saúde",
+          },
+        },
+        {
+          value: "C128438-5704",
+          label: {
+            en: "construction",
+            es: "construcción",
+            pt: "construção",
+          },
+        },
+        {
+          value: "C128438-5705",
+          label: {
+            en: "logistics",
+            es: "logística",
+            pt: "logística",
+          },
+        },
+        {
+          value: "C128438-5706",
+          label: {
+            en: "e-commerce",
+            es: "e-commerce",
+            pt: "e-commerce",
+          },
+        },
+        {
+          value: "C128438-5707",
+          label: {
+            en: "retail and trade",
+            es: "retail y comercio",
+            pt: "varejo e comércio",
+          },
+        },
+        {
+          value: "C128438-5708",
+          label: {
+            en: "agriculture",
+            es: "agricultura",
+            pt: "agricultura",
+          },
+        },
+        {
+          value: "C128438-5671",
+          label: {
+            en: "education",
+            es: "educación",
+            pt: "educação",
+          },
+        },
+        {
+          value: "C128438-5709",
+          label: {
+            en: "other",
+            es: "otro",
+            pt: "outro",
+          },
+        },
+      ],
+    },
+    /*services*/
+    {
+      type: "services",
+      label: {
+        en: "services",
+        es: "servicios",
+        pt: "servicios",
+      },
+      cats: [
+        {
+          value: "C128438-5638",
+          label: {
+            en: "banking",
+            es: "banca",
+            pt: "bancário",
+          },
+        },
+        {
+          value: "C128438-5679",
+          label: {
+            en: "consulting",
+            es: "consultoría",
+            pt: "consultoria",
+          },
+        },
+        {
+          value: "C128438-5653",
+          label: {
+            en: "software development",
+            es: "desarrollo de software",
+            pt: "desenvolvimento de software",
+          },
+        },
+        {
+          value: "C128438-5683",
+          label: {
+            en: "cloud",
+            es: "nube",
+            pt: "nuvem",
+          },
+        },
+        {
+          value: "C128438-5685",
+          label: {
+            en: "agrobusiness",
+            es: "agronegocio",
+            pt: "agronegócio",
+          },
+        },
+        {
+          value: "C128438-5687",
+          label: {
+            en: "chatbot",
+            es: "chatbot",
+            pt: "chatbot",
+          },
+        },
+        {
+          value: "C128438-5710",
+          label: {
+            en: "mobile device development",
+            es: "desarrollo de dispositivos móviles",
+            pt: "desenvolvimento dispositivo móveis",
+          },
+        },
+        {
+          value: "C128438-5711",
+          label: {
+            en: "government and public services",
+            es: "gobierno y servicios públicos",
+            pt: "governo e serviços públicos",
+          },
+        },
+        {
+          value: "C128438-5712",
+          label: {
+            en: "erp",
+            es: "erp",
+            pt: "erp",
+          },
+        },
+        {
+          value: "C128438-5713",
+          label: {
+            en: "human resources",
+            es: "recursos humanos",
+            pt: "recursos humanos",
+          },
+        },
+        {
+          value: "C128438-5714",
+          label: {
+            en: "communications",
+            es: "comunicaciones",
+            pt: "comunicações",
+          },
+        },
+        {
+          value: "C128438-5715",
+          label: {
+            en: "automation processes",
+            es: "procesos de automatización",
+            pt: "processos de automação",
+          },
+        },
+        {
+          value: "C128438-5716",
+          label: {
+            en: "management",
+            es: "management",
+            pt: "gerenciamento",
+          },
+        },
+        {
+          value: "C128438-5717",
+          label: {
+            en: "software factory",
+            es: "fábrica de software",
+            pt: "fábrica de software",
+          },
+        },
+        {
+          value: "C128438-5718",
+          label: {
+            en: "transportation",
+            es: "transporte",
+            pt: "transporte",
+          },
+        },
+        {
+          value: "C128438-5669",
+          label: {
+            en: "sap",
+            es: "sap",
+            pt: "sap",
+          },
+        },
+        {
+          value: "C128438-5664",
+          label: {
+            en: "digital transformation",
+            es: "transformación digital",
+            pt: "transformação digital",
+          },
+        },
+        {
+          value: "C128438-5667",
+          label: {
+            en: "automotive and manufacturing",
+            es: "automotriz y manufacturera",
+            pt: "automotriz y manufacturera",
+          },
+        },
+        {
+          value: "C128438-5709",
+          label: {
+            en: "other",
+            es: "otro",
+            pt: "outro",
+          },
+        },
+      ],
+    },
+    /*countries*/
+    {
+      type: "countries",
+      label: {
+        en: "countries",
+        es: "países",
+        pt: "países",
+      },
+      cats: [
+        {
+          value: "C128438-5681",
+          label: {
+            en: "argentina",
+            es: "argentina",
+            pt: "argentina",
+          },
+        },
+        {
+          value: "C128438-5677",
+          label: {
+            en: "brazil",
+            es: "brasil",
+            pt: "brasil",
+          },
+        },
+        {
+          value: "C128438-5719",
+          label: {
+            en: "chile",
+            es: "chile",
+            pt: "chile",
+          },
+        },
+        {
+          value: "C128438-5720",
+          label: {
+            en: "costa rica",
+            es: "costa rica",
+            pt: "costa rica",
+          },
+        },
+        {
+          value: "C128438-5721",
+          label: {
+            en: "cuba",
+            es: "cuba",
+            pt: "cuba",
+          },
+        },
+        {
+          value: "C128438-5722",
+          label: {
+            en: "mexico",
+            es: "méxico",
+            pt: "méxico",
+          },
+        },
+        {
+          value: "C128438-5723",
+          label: {
+            en: "paraguay",
+            es: "paraguay",
+            pt: "paraguai",
+          },
+        },
+        {
+          value: "C128438-5724",
+          label: {
+            en: "spain",
+            es: "españa",
+            pt: "espanha",
+          },
+        },
+        {
+          value: "C128438-5725",
+          label: {
+            en: "united states",
+            es: "estados unidos",
+            pt: "estados unidos",
+          },
+        },
+        {
+          value: "C128438-2671",
+          label: {
+            en: "uruguay",
+            es: "uruguay",
+            pt: "uruguai",
+          },
+        },
+      ],
+    },
+  ],
+};
