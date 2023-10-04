2f9cd59d10b0455b1b3d907b7761d6004b7eca32
Bruno Sastre <54677414+bsastregx@users.noreply.github.com>
2023-10-04 14:53:57 -0300

    update sticky banner

--- sticky-gx30/v2/assets/sticky-gx30.js
+++ sticky-gx30/v2/assets/sticky-gx30.js
@@ -1,1 +1,260 @@
-document.addEventListener("DOMContentLoaded",(function(){let n="is-not-mobile";/iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)&&(n="is-mobile");const e="https://gx30-sticky-banner.netlify.app/assets/";let t=document.documentElement.lang;t=t.includes("en")?"en":t.includes("es")?"es":t.includes("pt")?"pt":"en";const i={url:{en:"https://frontend.event.genexus.com/frontend.myregister.aspx",es:"https://frontend.event.genexus.com/frontend.myregister.aspx",pt:"https://frontend.event.genexus.com/frontend.myregister.aspx"},title:{en:"¡Reserve your spot at GX30, GeneXus Meeting!",es:"¡Reserva tu lugar en el GX30, Encuentro GeneXus!",pt:"¡Reserve o seu lugar no GX30, Encontro GeneXus!"},description:{en:"Register for free now.",es:"Regístrate gratis ya.",pt:"Registre-se gratuitamente agora."}},s=document.createElement("style");s.innerHTML=`\n @font-face {\n font-family: "Graphik Semibold";\n src: url(${e}graphik-semibold.woff);\n }\n @font-face {\n font-family: "Rubik Light";\n src: url(${e}rubik-light.ttf);\n }\n @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');\n #toggle-mobile {\n position: fixed;\n left: 16px;\n top: 16px;\n }\n /*--- Sticky banner ---*/\n #sticky-gx30 {\n text-decoration: none;\n color: #111111;\n position: fixed;\n bottom: 26px;\n left: 26px;\n transition: 100ms all;\n opacity: 1;\n width: 100%;\n max-width: 265px;\n z-index: 999999999;\n cursor: pointer;\n \n --main-padding: 16px;\n }\n #sticky-gx30:focus {\n outline:none;\n }\n #sticky-gx30:focus .sticky-gx30__article{\n border-color: #0F62FE;\n outline: 1px solid #0F62FE;\n }\n /*article*/\n #sticky-gx30 .sticky-gx30__article {\n background-color: #fff;\n border: 1px solid #111111;\n border-radius: 8px;\n box-sizing: border-box;\n padding: 8px;\n font-family: "Rubik Light", "Rubik Regular", Arial, sans-serif;\n }\n #sticky-gx30:hover {\n transform: scale(1.025);\n }\n #sticky-gx30.sticky-gx30--hidden {\n opacity: 0;\n left: 0;\n }\n /*header*/\n #sticky-gx30 .sticky-gx30__header {\n display: flex;\n flex-direction: column;\n }\n /*header top*/\n #sticky-gx30 .sticky-gx30__header-top {\n display: flex;\n justify-content: end;\n }\n #sticky-gx30 .sticky-gx30__close {\n background-color: transparent;\n border: 0;\n font-weight: bold;\n font-size: 18px;\n cursor: pointer;\n padding:0;\n line-height:0;\n }\n #sticky-gx30 .sticky-gx30__close:hover {\n transform: scale(1.25);\n }\n #sticky-gx30 .sticky-gx30__close:focus {\n outline: none;\n }\n /*header bottom*/\n #sticky-gx30 .sticky-gx30__header-bottom {\n padding: 0 var(--main-padding);\n }\n #sticky-gx30 .sticky-gx30__logo {\n width: 80px;\n height: auto;\n }\n /*main*/\n #sticky-gx30 .sticky-gx30__main {\n padding: var(--main-padding);\n }\n /*title*/\n #sticky-gx30 .sticky-gx30__title {\n font-size:20px;\n line-height:1.3em;\n font-family: "Graphik Semibold", Arial, sans-serif;\n margin-block-start: 0;\n margin-block-end: 8px;\n }\n /*paragraph*/\n #sticky-gx30 .sticky-gx30__caption {\n font-size: 17px;\n font-weight: 300;\n line-height:1.425em;\n font-family: 'Rubik', sans-serif;\n }\n /*--- Mobile version ---*/\n #sticky-gx30.is-mobile {\n max-width: calc(100% - 52px);\n }\n #sticky-gx30.is-mobile .sticky-gx30__article {\n padding: 0;\n }\n #sticky-gx30.is-mobile .sticky-gx30__header {\n padding: var(--main-padding) var(--main-padding) 0 var(--main-padding);\n flex-direction: row-reverse;\n justify-content: space-between;\n align-items: center;\n }\n #sticky-gx30.is-mobile .sticky-gx30__header-bottom {\n display: flex;\n align-items: center;\n padding: 0;\n }\n #sticky-gx30.is-mobile .sticky-gx30__logo {\n width: 40px;\n height: auto;\n }\n #sticky-gx30.is-mobile .sticky-gx30__close {\n position: relative;\n top: -13px;\n right: -8px;\n }\n #sticky-gx30.is-mobile .sticky-gx30__footer {\n display: flex;\n align-items: end;\n }\n #sticky-gx30.is-mobile .sticky-gx30__caption {\n margin: 0;\n }\n \n`,document.head.appendChild(s);const o=`\n <article class="sticky-gx30__article">\n <header class="sticky-gx30__header">\n <div class="sticky-gx30__header-top">\n <button class="sticky-gx30__close"><img\n class="sticky-gx30__close-img"\n src="${e}close.svg"\n alt="GX30 logo"\n /></button>\n </div>\n <div class="sticky-gx30__header-bottom">\n <img\n class="sticky-gx30__logo"\n src="${e}gx-30-squared.svg"\n alt="GX30 logo"\n />\n </div>\n </header>\n <main class="sticky-gx30__main">\n <h1 class="sticky-gx30__title">${i.title[t]}</h1>\n <footer class="sticky-gx30__footer">\n <div class="sticky-gx30__footer-top">\n <p class="sticky-gx30__caption">${i.description[t]}</p>\n </div>\n <div class="sticky-gx30__footer-bottom">\n <img\n class="sticky-gx30__arrow"\n src="${e}arrow.svg"\n alt="arrow icon"\n />\n </div>\n </footer>\n </main>\n </article>\n`,a=document.createElement("a");a.setAttribute("id","sticky-gx30"),a.classList.add("sticky-gx30--hidden",n),a.setAttribute("target","_blank"),a.setAttribute("href",i.url[t]),document.body.appendChild(a),a.innerHTML=o,setTimeout((()=>{a.classList.remove("sticky-gx30--hidden")}),1e3);const c=()=>{a.classList.add("sticky-gx30--hidden"),setTimeout((()=>{a.remove()}),1e3)},r=document.querySelector(".sticky-gx30__close");r&&r.addEventListener("click",(function(n){n.preventDefault(),c()})),a.addEventListener("click",c)}));
+document.addEventListener("DOMContentLoaded", function () {
+  // Code inside this function will run when the DOM is fully loaded and ready.
+  // You can safely manipulate the DOM or execute JavaScript code here.
+
+  /*=== Detect is mobile ====*/
+  const isMobile =
+    /iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+  let isMobileClass = "is-not-mobile";
+  if (isMobile) {
+    isMobileClass = "is-mobile";
+  }
+
+  /*=== Data ====*/
+  /*local url*/
+  //const baseUrlAssets = "./assets/";
+  /*netlify url*/
+  const baseUrlAssets = "https://gx30-sticky-banner.netlify.app/assets/";
+  let pageLang = document.documentElement.lang;
+  if (pageLang.includes("en")) {
+    pageLang = "en";
+  } else if (pageLang.includes("es")) {
+    pageLang = "es";
+  } else if (pageLang.includes("pt")) {
+    pageLang = "pt";
+  } else {
+    //default english
+    pageLang = "en";
+  }
+
+  const gx30 = {
+    url: {
+      en: "https://frontend.event.genexus.com/frontend.myregister.aspx",
+      es: "https://frontend.event.genexus.com/frontend.myregister.aspx",
+      pt: "https://frontend.event.genexus.com/frontend.myregister.aspx",
+    },
+    title: {
+      en: "¡Reserve your spot at GX30, GeneXus Meeting!",
+      es: "¡Reserva tu lugar en el GX30, Encuentro GeneXus!",
+      pt: "¡Reserve o seu lugar no GX30, Encontro GeneXus!",
+    },
+    description: {
+      en: "Register for free now.",
+      es: "Regístrate gratis ya.",
+      pt: "Registre-se gratuitamente agora.",
+    },
+  };
+
+  /*=== Styles ====*/
+  const gx30StyleTag = document.createElement("style");
+  gx30StyleTag.innerHTML = `
+    @font-face {
+      font-family: "Graphik Semibold";
+      src: url(${baseUrlAssets}graphik-semibold.woff);
+    }
+    @font-face {
+      font-family: "Rubik Light";
+      src: url(${baseUrlAssets}rubik-light.ttf);
+    }
+    @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');
+    #toggle-mobile {
+      position: fixed;
+      left: 16px;
+      top: 16px;
+    }
+    /*--- Sticky banner ---*/
+    #sticky-gx30 {
+      text-decoration: none;
+      color: #111111;
+      position: fixed;
+      bottom: 26px;
+      left: 26px;
+      transition: 100ms all;
+      opacity: 1;
+      width: 100%;
+      max-width: 265px;
+      z-index: 999999999;
+      cursor: pointer;
+    
+      --main-padding: 16px;
+    }
+    #sticky-gx30:focus {
+      outline:none;
+    }
+    #sticky-gx30:focus .sticky-gx30__article{
+      border-color: #0F62FE;
+      outline: 1px solid #0F62FE;
+    }
+    /*article*/
+    #sticky-gx30 .sticky-gx30__article {
+      background-color: #fff;
+      border: 1px solid #111111;
+      border-radius: 8px;
+      box-sizing: border-box;
+      padding: 8px;
+      font-family: "Rubik Light", "Rubik Regular", Arial, sans-serif;
+    }
+    #sticky-gx30:hover {
+      transform: scale(1.025);
+    }
+    #sticky-gx30.sticky-gx30--hidden {
+      opacity: 0;
+      left: 0;
+    }
+    /*header*/
+    #sticky-gx30 .sticky-gx30__header {
+      display: flex;
+      flex-direction: column;
+    }
+    /*header top*/
+    #sticky-gx30 .sticky-gx30__header-top {
+      display: flex;
+      justify-content: end;
+    }
+    #sticky-gx30 .sticky-gx30__close {
+      background-color: transparent;
+      border: 0;
+      font-weight: bold;
+      font-size: 18px;
+      cursor: pointer;
+      padding:0;
+      line-height:0;
+    }
+    #sticky-gx30 .sticky-gx30__close:hover {
+      transform: scale(1.25);
+    }
+    #sticky-gx30 .sticky-gx30__close:focus {
+      outline: none;
+    }
+    /*header bottom*/
+    #sticky-gx30 .sticky-gx30__header-bottom {
+      padding: 0 var(--main-padding);
+    }
+    #sticky-gx30 .sticky-gx30__logo {
+      width: 80px;
+      height: auto;
+    }
+    /*main*/
+    #sticky-gx30 .sticky-gx30__main {
+      padding: var(--main-padding);
+    }
+    /*title*/
+    #sticky-gx30 .sticky-gx30__title {
+      font-size:20px;
+      line-height:1.3em;
+      font-family: "Graphik Semibold", Arial, sans-serif;
+      margin-block-start: 0;
+      margin-block-end: 8px;
+    }
+    /*paragraph*/
+    #sticky-gx30 .sticky-gx30__caption {
+      font-size: 17px;
+      font-weight: 300;
+      line-height:1.425em;
+      font-family: 'Rubik', sans-serif;
+    }
+    /*--- Mobile version ---*/
+    #sticky-gx30.is-mobile {
+      max-width: calc(100% - 52px);
+    }
+    #sticky-gx30.is-mobile .sticky-gx30__article {
+      padding: 0;
+    }
+    #sticky-gx30.is-mobile .sticky-gx30__header {
+      padding: var(--main-padding) var(--main-padding) 0 var(--main-padding);
+      flex-direction: row-reverse;
+      justify-content: space-between;
+      align-items: center;
+    }
+    #sticky-gx30.is-mobile .sticky-gx30__header-bottom {
+      display: flex;
+      align-items: center;
+      padding: 0;
+    }
+    #sticky-gx30.is-mobile .sticky-gx30__logo {
+      width: 40px;
+      height: auto;
+    }
+    #sticky-gx30.is-mobile .sticky-gx30__close {
+      position: relative;
+      top: -13px;
+      right: -8px;
+    }
+    #sticky-gx30.is-mobile .sticky-gx30__footer {
+      display: flex;
+      align-items: end;
+    }
+    #sticky-gx30.is-mobile .sticky-gx30__caption {
+      margin: 0;
+    }
+    
+  `;
+  document.head.appendChild(gx30StyleTag);
+
+  /*=== HTML ====*/
+  const gx30Html = `
+      <article class="sticky-gx30__article">
+        <header class="sticky-gx30__header">
+          <div class="sticky-gx30__header-top">
+            <button class="sticky-gx30__close"><img
+            class="sticky-gx30__close-img"
+            src="${baseUrlAssets}close.svg"
+            alt="GX30 logo"
+          /></button>
+          </div>
+          <div class="sticky-gx30__header-bottom">
+            <img
+              class="sticky-gx30__logo"
+              src="${baseUrlAssets}gx-30-squared.svg"
+              alt="GX30 logo"
+            />
+          </div>
+        </header>
+        <main class="sticky-gx30__main">
+          <h1 class="sticky-gx30__title">${gx30.title[pageLang]}</h1>
+          <footer class="sticky-gx30__footer">
+            <div class="sticky-gx30__footer-top">
+              <p class="sticky-gx30__caption">${gx30.description[pageLang]}</p>
+            </div>
+            <div class="sticky-gx30__footer-bottom">
+              <img
+                class="sticky-gx30__arrow"
+                src="${baseUrlAssets}arrow.svg"
+                alt="arrow icon"
+              />
+            </div>
+          </footer>
+        </main>
+      </article>
+  `;
+
+  /*=== Scripts ====*/
+  const stickyLink = document.createElement("a");
+  stickyLink.setAttribute("id", "sticky-gx30");
+  stickyLink.classList.add("sticky-gx30--hidden", isMobileClass);
+  stickyLink.setAttribute("target", "_blank");
+  stickyLink.setAttribute("href", gx30.url[pageLang]);
+  document.body.appendChild(stickyLink);
+  stickyLink.innerHTML = gx30Html;
+  setTimeout(() => {
+    stickyLink.classList.remove("sticky-gx30--hidden");
+  }, 1000);
+
+  const removeStickyBanner = () => {
+    stickyLink.classList.add("sticky-gx30--hidden");
+    setTimeout(() => {
+      stickyLink.remove();
+    }, 1000);
+  };
+
+  const closeButton = document.querySelector(".sticky-gx30__close");
+  if (closeButton) {
+    closeButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      removeStickyBanner();
+    });
+  }
+  stickyLink.addEventListener("click", removeStickyBanner);
+});
